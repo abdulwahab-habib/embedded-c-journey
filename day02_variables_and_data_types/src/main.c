ebb4335d48b93d036d99a5d30c526d146dde9d47
Abdulwahab Habib <hoabdulwahab@student.lautech.edu.ng>
2026-03-15 14:47:29 +0100

    implemented basic data-types

---
+++ day02_variables_and_data_types/src/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+  int temperature = 25;
+  int pressure = 101325;
+  float humidity = 50.0;
+
+  printf("Temperature: %d °C\n", temperature);
+  printf("Pressure: %d Pa\n", pressure);
+  printf("Reletive Humidity: %.1f%%\n", humidity);
+}
